82768b2064c259cfb061904724c6e3ac170ae001
Yi Cao <yicao928@gmail.com>
2023-05-16 14:38:42 -0400

    feat: add find customized path api

--- EleNA/api/map_reader.py
+++ EleNA/api/map_reader.py
@@ -13,11 +13,18 @@ osm = esy.osm.pbf.File(map_file_path)
 map = OSM(map_file_path)
 nodes, edges = map.get_network(nodes=True, network_type="walking")
 
+try:
+    with open('data/altitude_dict.json', 'r') as f:
+        altitude_json = json.load(f)
+    node_altitude = dict()
+    for key, value in altitude_json.items():
+        node_altitude[int(key)] = value
+
+except:
+    node_altitude = dict()
+
 with open('data/graph.json', 'r') as f:
     whole_graph = json.load(f)
-
-# connect to database
-connection = sqlite3.connect('data/geo_data')
 
 
 def search_node_by_tag(search_tag: str, search_value: str):
@@ -81,9 +88,82 @@ def dijkstra(start: int, end: int, node_id_list: dict):
     return path, shortest_distance
 
 
+# dijkstra algorithm to find shortest path
+def dijkstra_edited(start: int, end: int, node_id_list: dict, distance_limit: float, x: int):
+    # Initialize distance to each node as infinity
+    distance = {node: float('inf') for node in node_id_list}
+    # The distance to the starting node is 0
+    distance[start] = 0
+
+    # item in queue: (elevation, (path, distance))
+    queue = [(0, ([start], 0))]
+
+    while queue:
+        # Get the next closest node to visit
+        current_elevation, (current_path, current_distance) = heapq.heappop(queue)
+        current_elevation = current_elevation * x
+        current_node = current_path[-1]
+
+        # If we've already found a shorter path to this node, skip it
+        if current_distance > distance[current_node]:
+            continue
+
+        # Check the distance to all neighboring nodes
+        for neighbor, weight in whole_graph[str(current_node)].items():
+            if int(neighbor) == end:
+                current_path.append(end)
+                current_elevation += get_elevation(current_node, int(end))
+                current_distance += weight
+                with open('data/altitude_dict.json', 'w') as convert_file:
+                    convert_file.write(json.dumps(node_altitude))
+                return current_path, current_elevation, current_distance
+
+            if int(neighbor) not in node_id_list or int(neighbor) in current_path:
+                continue
+            # Calculate the distance to this neighbor through the current node
+            new_distance = distance[current_node] + weight
+
+            # If we've found a shorter path to this neighbor, update our records
+            if new_distance < distance[int(neighbor)] and new_distance < distance_limit:
+                if new_distance < distance[int(neighbor)]:
+                    distance[int(neighbor)] = new_distance
+                new_elevation = current_elevation + get_elevation(current_node, int(neighbor))
+                new_path = [i for i in current_path]
+                new_path.append(int(neighbor))
+                heapq.heappush(queue, (new_elevation * x, (new_path, new_distance)))
+
+    with open('altitude_dict.json', 'w') as convert_file:
+        convert_file.write(json.dumps(node_altitude))
+    return None, None, None
+
+
+def get_elevation(node1: int, node2: int) -> float:
+    latlon = node_id_to_latlon([node1, node2])
+    lat1 = latlon[0][0]
+    lon1 = latlon[0][1]
+    lat2 = latlon[1][0]
+    lon2 = latlon[1][1]
+
+    if node1 in node_altitude.keys():
+        altitude1 = float(node_altitude[node1])
+    else:
+        altitude1 = float(get_altitude(lat1, lon1))
+        node_altitude[node1] = altitude1
+    if node2 in node_altitude.keys():
+        altitude2 = float(node_altitude[node2])
+    else:
+        altitude2 = float(get_altitude(lat2, lon2))
+        node_altitude[node2] = altitude2
+
+    if altitude2 - altitude1 <= 0.0:
+        return 0.0
+    return altitude2 - altitude1
+
+
 # get altitude from given latitude and longitude
 def get_altitude(latitude: str, longitude: str) -> str:
     # try to get data from database
+    connection = sqlite3.connect('data/geo_data')
     cursor = connection.cursor()
     get_altitude_sql = 'SELECT elevation FROM elevation_data WHERE latitude=? AND longitude=?'
     cursor.execute(get_altitude_sql, (latitude, longitude))
@@ -105,11 +185,12 @@ def get_altitude(latitude: str, longitude: str) -> str:
     connection.commit()
 
     cursor.close()
+    connection.close()
     return altitude
 
 
 # find the nearest node in graph to target location
-def get_nearest_node(lat: str, lon: str) -> str:
+def get_nearest_node(lat: float, lon: float) -> str:
     node_to_search = nodes[
         (nodes.lat >= lat - 0.01) & (nodes.lat <= lat + 0.01) & (nodes.lon >= lon - 0.01) & (nodes.lon <= lat + 0.01)]
 
@@ -126,9 +207,9 @@ def get_nearest_node(lat: str, lon: str) -> str:
 
 
 # find a smaller area to calculate the shortest path
-def get_sub_area(start_lat: float, start_lon: float, end_lat: float, end_lon: float) -> list:
-    d_lat = np.abs(start_lat - end_lat)
-    d_lon = np.abs(start_lon - end_lon)
+def get_sub_area(start_lat: float, start_lon: float, end_lat: float, end_lon: float, coefficient: float) -> list:
+    d_lat = np.abs(start_lat - end_lat) * np.sqrt(coefficient)
+    d_lon = np.abs(start_lon - end_lon) * np.sqrt(coefficient)
     max_lat = np.max([start_lat + d_lat, end_lat + d_lat])
     min_lat = np.min([start_lat - d_lat, end_lat - d_lat])
     max_lon = np.max([start_lon + d_lat, end_lat + d_lon])
