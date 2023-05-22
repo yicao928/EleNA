cb42a0b7953b1ea1d230db3f5880de09b5584bdb
Yi Cao <yicao928@gmail.com>
2023-05-22 16:30:37 -0400

    delete find_shortest_path api

--- EleNA/api/urls.py
+++ EleNA/api/urls.py
@@ -5,6 +5,5 @@ from .find_customized_path import api_find_customized_path
 
 urlpatterns = [
     path('search_node_by_tag', api_search_node_by_tag, name='search_node_by_tag'),
-    path('find_shortest_path', api_find_shortest_path, name='find_shortest_path'),
     path('find_customized_path', api_find_customized_path, name='find_customized_path'),
 ]
